e09f7251519779178fb0865e55e82187a5b73949
junghanseam <hsjung@nexmore.co.kr>
2024-03-28 17:16:20 +0900

    원복

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,8 @@ import './index.css';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 
-console.log(document.location.pathname);
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<HashRouter basename={`${document.location.pathname === '/' ? '/' : `${document.location.pathname}/`}`}>
+	<HashRouter>
 		<App />
 	</HashRouter>,
 );
