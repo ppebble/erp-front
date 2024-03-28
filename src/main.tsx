e4f2c3c4376faa32440720bf6d75321317857126
HyeunUk <huju@nexmore.co.kr>
2024-03-28 16:13:20 +0900

    router rollback

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<HashRouter basename="/">
+	<HashRouter>
 		<App />
 	</HashRouter>,
 );
