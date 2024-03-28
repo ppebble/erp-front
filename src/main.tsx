4736b332ed1735f9e6784264d714fcf1a45af003
junghanseam <hsjung@nexmore.co.kr>
2024-03-28 17:07:16 +0900

    basename 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import './index.css';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 
+console.log(document.location.pathname);
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<HashRouter basename={`${document.location.pathname === '/' ? '/' : `${document.location.pathname}`}`}>
+	<HashRouter basename={`${document.location.pathname === '/' ? '/' : `${document.location.pathname}/#`}`}>
 		<App />
 	</HashRouter>,
 );
