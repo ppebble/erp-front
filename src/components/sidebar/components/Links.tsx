05799bf00c7532d85450122c56673c916cec9ebc
ask135799@naver.com <ask135799@naver.com>
2024-04-01 14:12:52 +0900

    사이드네비 <>태그 <Fragment>로 변경

--- src/components/sidebar/components/Links.tsx
+++ src/components/sidebar/components/Links.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import DashIcon from '../../../components/icons/DashIcon';
 import { MdHome } from 'react-icons/md';
@@ -21,51 +21,49 @@ export const SidebarLinks = (props: { routes: RoutesType[] }): JSX.Element => {
 
 	const createLinks = (routes: RoutesType[]) => {
 		return routes.map((route, index) => {
-			if (route.layout === '/admin' || route.layout === '/auth' || route.layout === '/rtl') {
-				return (
-					<>
-						<div className="relative mb-3 flex hover:cursor-pointer" onClick={() => setOpenPath(route.path)} key={route.path}>
-							<li className="my-[3px] flex cursor-pointer items-center px-8" key={route.path + 'li'}>
-								<span className={`${openPath === route.path ? 'font-bold text-brand-500 dark:text-white' : 'font-medium text-gray-600'}`}>
-									{route.icon ? route.icon : <DashIcon />}{' '}
-								</span>
-								<p
-									className={`leading-1 ml-4 flex ${openPath === route.path ? 'font-bold text-navy-700 dark:text-white' : 'font-medium text-gray-600'}`}
+			return (
+				<Fragment key={route.path}>
+					<div className="relative mb-3 flex hover:cursor-pointer" onClick={() => setOpenPath(route.path)}>
+						<li className="my-[3px] flex cursor-pointer items-center px-8" key={route.path + 'li'}>
+							<span className={`${openPath === route.path ? 'font-bold text-brand-500 dark:text-white' : 'font-medium text-gray-600'}`}>
+								{route.icon ? route.icon : <DashIcon />}{' '}
+							</span>
+							<p
+								className={`leading-1 ml-4 flex ${openPath === route.path ? 'font-bold text-navy-700 dark:text-white' : 'font-medium text-gray-600'}`}
+							>
+								{route.name}
+							</p>
+						</li>
+						{/* {isOpenPath === route.path ? <div className="absolute right-0 top-px h-9 w-1 rounded-lg bg-brand-500 dark:bg-brand-400" /> : null} */}
+					</div>
+					{route.child?.map((child, index) => {
+						return (
+							<Link key={child.path} to={child.layout + '/' + child.path}>
+								<div
+									key={child.path + 'div'}
+									className={`${openPath !== route.path ? 'invisible h-0 mb-0 ' : 'relative mb-3 flex hover:cursor-pointer ml-10'}`}
 								>
-									{route.name}
-								</p>
-							</li>
-							{/* {isOpenPath === route.path ? <div className="absolute right-0 top-px h-9 w-1 rounded-lg bg-brand-500 dark:bg-brand-400" /> : null} */}
-						</div>
-						{route.child?.map((child, index) => {
-							return (
-								<Link key={child.path} to={child.layout + '/' + child.path}>
-									<div
-										key={child.path + 'div'}
-										className={`${openPath !== route.path ? 'invisible h-0 mb-0 ' : 'relative mb-3 flex hover:cursor-pointer ml-10'}`}
-									>
-										<li className="my-[3px] flex cursor-pointer items-center px-8">
-											<span
-												className={`${activeRoute(child.path) === true ? 'font-bold text-brand-500 dark:text-white' : 'font-medium text-gray-600'}`}
-											>
-												{child.icon ? child.icon : <DashIcon />}{' '}
-											</span>
-											<p
-												className={`leading-1 ml-4 flex ${
-													activeRoute(child.path) === true ? 'font-bold text-navy-700 dark:text-white' : 'font-medium text-gray-600'
-												}`}
-											>
-												{child.name}
-											</p>
-										</li>
-										{activeRoute(child.path) ? <div className="absolute right-0 top-px h-9 w-1 rounded-lg bg-brand-500 dark:bg-brand-400" /> : null}
-									</div>
-								</Link>
-							);
-						})}
-					</>
-				);
-			}
+									<li className="my-[3px] flex cursor-pointer items-center px-8">
+										<span
+											className={`${activeRoute(child.path) === true ? 'font-bold text-brand-500 dark:text-white' : 'font-medium text-gray-600'}`}
+										>
+											{child.icon ? child.icon : <DashIcon />}{' '}
+										</span>
+										<p
+											className={`leading-1 ml-4 flex ${
+												activeRoute(child.path) === true ? 'font-bold text-navy-700 dark:text-white' : 'font-medium text-gray-600'
+											}`}
+										>
+											{child.name}
+										</p>
+									</li>
+									{activeRoute(child.path) ? <div className="absolute right-0 top-px h-9 w-1 rounded-lg bg-brand-500 dark:bg-brand-400" /> : null}
+								</div>
+							</Link>
+						);
+					})}
+				</Fragment>
+			);
 		});
 	};
 	// BRAND
