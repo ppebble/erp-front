f5674f2213b59b9d698486e011d17891edefdd40
junghanseam <hsjung@nexmore.co.kr>
2024-04-11 17:47:53 +0900

    인력현황 - 요약통계 수정

---
+++ src/pages/topic/summary/chart/funnel.tsx
@@ -0,0 +1,76 @@
+import { Card } from '@chakra-ui/react';
+import { ResponsiveFunnel } from '@nivo/funnel';
+
+const Funnel = () => {
+	const data = [
+		{
+			id: 'level_7',
+			value: 1,
+			label: '대표',
+		},
+		{
+			id: 'level_6',
+			value: 2,
+			label: '상무, 이사',
+		},
+		{
+			id: 'level_5',
+			value: 2,
+			label: '부장',
+		},
+		{
+			id: 'level_4',
+			value: 4,
+			label: '차장',
+		},
+		{
+			id: 'level_3',
+			value: 2,
+			label: '과장',
+		},
+		{
+			id: 'level_2',
+			value: 6,
+			label: '대리',
+		},
+		{
+			id: 'level_1',
+			value: 7,
+			label: '주임',
+		},
+	];
+
+	const detailView = (value: any) => {
+		console.log(value.data.label);
+	};
+
+	return (
+		<Card className="w-full h-full p-4">
+			<div className="w-full h-full">
+				<ResponsiveFunnel
+					data={data}
+					margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+					shapeBlending={0.2}
+					valueFormat=" >-.0r"
+					colors={{ scheme: 'paired' }}
+					borderWidth={10}
+					enableLabel={false}
+					onClick={(value) => detailView(value)}
+					labelColor={{
+						from: 'color',
+						modifiers: [['darker', 3]],
+					}}
+					beforeSeparatorLength={100}
+					beforeSeparatorOffset={20}
+					afterSeparatorLength={100}
+					afterSeparatorOffset={20}
+					currentPartSizeExtension={10}
+					currentBorderWidth={10}
+					motionConfig="wobbly"
+				/>
+			</div>
+		</Card>
+	);
+};
+
+export default Funnel;
---
+++ src/pages/topic/summary/chart/pie.tsx
@@ -0,0 +1,94 @@
+import { Card } from '@chakra-ui/react';
+import { ResponsivePie } from '@nivo/pie';
+
+const Pie = () => {
+	const data = [
+		{
+			id: '주임',
+			value: 7,
+		},
+		{
+			id: '대리',
+			value: 6,
+		},
+		{
+			id: '과장',
+			value: 2,
+		},
+		{
+			id: '차장',
+			value: 4,
+		},
+		{
+			id: '부장',
+			value: 2,
+		},
+		{
+			id: '상무, 이사',
+			value: 2,
+		},
+		{
+			id: '대표',
+			value: 1,
+		},
+	];
+
+	return (
+		<Card className="w-full p-4 h-full">
+			<div className="w-full h-full">
+				<ResponsivePie
+					data={data}
+					margin={{ top: 20, right: 20, bottom: 70, left: 20 }}
+					valueFormat=" >-"
+					innerRadius={0.5}
+					fit={false}
+					activeInnerRadiusOffset={10}
+					activeOuterRadiusOffset={20}
+					colors={{ scheme: 'set1' }}
+					borderColor={{
+						from: 'color',
+						modifiers: [['brighter', 0.2]],
+					}}
+					enableArcLinkLabels={false}
+					arcLinkLabelsSkipAngle={10}
+					arcLinkLabelsTextColor="#333333"
+					arcLinkLabelsThickness={2}
+					arcLinkLabelsColor={{ from: 'color' }}
+					// arcLabel={(e) => `${e.id}`}
+					arcLabel={(e) => ``}
+					arcLabelsTextColor={{
+						from: 'color',
+						modifiers: [['darker', 3]],
+					}}
+					legends={[
+						{
+							anchor: 'bottom',
+							direction: 'row',
+							justify: false,
+							translateX: 0,
+							translateY: 56,
+							itemsSpacing: 0,
+							itemWidth: 100,
+							itemHeight: 18,
+							itemTextColor: '#999',
+							itemDirection: 'left-to-right',
+							itemOpacity: 1,
+							symbolSize: 18,
+							symbolShape: 'circle',
+							effects: [
+								{
+									on: 'hover',
+									style: {
+										itemTextColor: '#000',
+									},
+								},
+							],
+						},
+					]}
+				/>
+			</div>
+		</Card>
+	);
+};
+
+export default Pie;
