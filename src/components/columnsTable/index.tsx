b77a2c4bfb13042ea534db089466ce734654862d
junghanseam <hsjung@nexmore.co.kr>
2024-05-24 18:52:51 +0900

    게시판 타입 컴포넌트화

---
+++ src/components/columnsTable/index.tsx
@@ -0,0 +1,227 @@
+import { useMemo, useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { SortingState, createColumnHelper, flexRender, getCoreRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table';
+import { Flex, Input, InputGroup, InputRightElement, Select, Spacer } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+import { ProfileService } from '../../services/profileService';
+
+type searchType = {
+	option: string;
+	input: string;
+};
+
+type ColumnsTableProps = {
+	columns: any;
+	searchItem: any;
+	list: any;
+	show: number;
+	search: searchType;
+	setSearch: (state: searchType) => void;
+	filter: any;
+};
+
+const ColumnsTable = ({ columns, searchItem, list, show, search, setSearch, filter }: ColumnsTableProps) => {
+	useQuery('getProfileList', ProfileService().getProfileList);
+	const [row] = useState(show);
+
+	const [totalPage, setTotalPage] = useState(Math.ceil(list.length / row));
+
+	const splitList = () => {
+		const array = [];
+		for (let i = 0; i < totalPage; i += 1) {
+			array.push(list.slice(i * row, (i + 1) * row));
+		}
+		return array;
+	};
+
+	const [profileListSplit] = useState<any>(splitList()); // 10개(row)씩 나눈 리스트
+	const [searchSplit, setSearchSplit] = useState<any>(); // 검색 결과 리스트
+
+	const [data, setData] = useState(profileListSplit[0]);
+
+	const [sorting, setSorting] = useState<SortingState>([]);
+	const [currentPage, setCurrentPage] = useState(0);
+
+	const previousPage = () => {
+		setCurrentPage(currentPage - 1);
+	};
+
+	const nextPage = () => {
+		setCurrentPage(currentPage + 1);
+	};
+
+	const changePage = (page: number) => {
+		setCurrentPage(page);
+	};
+
+	const [customPagination, setCustomPagination] = useState<any[]>();
+
+	useMemo(() => {
+		const arr = [];
+		for (let i = 0; i < totalPage; i += 1) {
+			arr[i] = i;
+		}
+		setCustomPagination(arr);
+	}, [totalPage]);
+
+	const table = useReactTable({
+		data,
+		columns,
+		state: {
+			sorting,
+		},
+		onSortingChange: setSorting,
+		getCoreRowModel: getCoreRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		debugTable: true,
+	});
+
+	const onSearch = (e: any) => {
+		const { id, value } = e.target;
+		setSearch({ ...search, [id]: value });
+	};
+
+	useEffect(() => {
+		if (search.input) {
+			setData(searchSplit[currentPage]);
+		} else {
+			setData(profileListSplit[currentPage]);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentPage]);
+
+	useEffect(() => {
+		if (search) {
+			setCurrentPage(0);
+			if (filter) {
+				if (search.input) {
+					const array = [];
+					if (filter.length >= 10) {
+						for (let i = 0; i < Math.ceil(filter.length / row); i += 1) {
+							array.push(filter.slice(i * row, (i + 1) * row));
+						}
+						setSearchSplit(array);
+						setData(array[0]);
+					} else {
+						setSearchSplit(filter);
+						setData(filter);
+					}
+				} else {
+					setData(profileListSplit[currentPage]);
+				}
+
+				setTotalPage(Math.ceil(filter.length / row));
+			}
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [search, filter]);
+
+	return (
+		<div>
+			<div className="mt-8 mx-[3rem] min-h-[600px]">
+				<table className="w-full">
+					<thead>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<tr key={headerGroup.id} className="!border-px !border-gray-400">
+								{headerGroup.headers.map((header) => {
+									return (
+										<th
+											key={header.id}
+											colSpan={header.colSpan}
+											onClick={header.column.getToggleSortingHandler()}
+											className="cursor-pointer border-b-[1px] border-gray-200 pt-4 pb-2 pr-4 text-start"
+											style={{ width: header.getSize() }}
+										>
+											<div className="items-center justify-between text-xs text-gray-200">
+												<p className="text-lg font-bold text-gray-600 dark:text-white">
+													{header.id}
+													{{
+														asc: '▲',
+														desc: '▼',
+													}[header.column.getIsSorted() as string] ?? null}
+												</p>
+											</div>
+										</th>
+									);
+								})}
+							</tr>
+						))}
+					</thead>
+					<tbody>
+						{table.getRowModel().rows.map((rows) => {
+							return (
+								<tr key={rows.id}>
+									{rows.getVisibleCells().map((cell) => {
+										return (
+											<td key={cell.id} className="border-white/0 py-3 pr-4">
+												{flexRender(cell.column.columnDef.cell, cell.getContext())}
+											</td>
+										);
+									})}
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+
+			<Flex className="mt-[2rem] mx-[2rem]">
+				{/* 검색 */}
+				<InputGroup className="mb-2">
+					<div className="!w-[8rem]">
+						<Select id="option" defaultValue="name" onChange={(e) => onSearch(e)}>
+							{searchItem.map((item: any) => (
+								<option key={item.option} value={item.option}>
+									{item.value}
+								</option>
+							))}
+						</Select>
+					</div>
+					<Input id="input" className="ml-[2rem]" onChange={(e) => onSearch(e)} />
+					<InputRightElement>
+						<SearchIcon />
+					</InputRightElement>
+				</InputGroup>
+				<Spacer />
+
+				{/* 페이지 */}
+				<div className="w-full  flex justify-center sm:justify-end flex-col sm:flex-row gap-5 items-center">
+					<div className="flex">
+						<ul className="flex justify-center items-center gap-x-[10px] z-30" role="navigation" aria-label="Pagination">
+							<li
+								className={` prev-btn flex items-center justify-center w-[36px] rounded-[6px] h-[36px] border-[1px] border-solid border-[#E4E4EB] disabled] ${
+									currentPage === 0 ? 'bg-[#cccccc] pointer-events-none' : ' cursor-pointer'
+								}`}
+								onClick={previousPage}
+							>
+								<MdChevronLeft />
+							</li>
+							{customPagination?.map((_data, index) => (
+								<li
+									className={`flex items-center justify-center w-[36px] rounded-[6px] h-[34px] border-[1px] border-solid border-[2px] bg-[#FFFFFF] cursor-pointer ${
+										currentPage === index ? 'text-blue-600  border-sky-500' : 'border-[#E4E4EB] '
+									}`}
+									onClick={() => changePage(index)}
+									key={_data}
+								>
+									{index + 1}
+								</li>
+							))}
+							<li
+								className={`flex items-center justify-center w-[36px] rounded-[6px] h-[36px] border-[1px] border-solid border-[#E4E4EB] ${
+									currentPage === totalPage - 1 ? 'bg-[#cccccc] pointer-events-none' : ' cursor-pointer'
+								}`}
+								onClick={nextPage}
+							>
+								<MdChevronRight />
+							</li>
+						</ul>
+					</div>
+				</div>
+			</Flex>
+		</div>
+	);
+};
+
+export default ColumnsTable;
--- src/pages/topic/workforce/index.tsx
+++ src/pages/topic/workforce/index.tsx
@@ -1,60 +1,19 @@
-import { useMemo, useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
-import { SortingState, createColumnHelper, flexRender, getCoreRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table';
-import { Flex, Input, InputGroup, InputRightElement, Select, Spacer } from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
+import { createColumnHelper } from '@tanstack/react-table';
+import ColumnsTable from '../../../components/columnsTable';
+import { profileList as profileListType } from '../../../network/response/profileList';
 import { ProfileService } from '../../../services/profileService';
 import useProfile from '../../../store/useProfile';
-import { profileList as profileListType } from '../../../network/response/profileList';
 import Card from '../../../components/card';
 
 const Workforce = () => {
 	useQuery('getProfileList', ProfileService().getProfileList);
-	const { profileList } = useProfile(); // 전체 리스트
-	const row = 10;
-
-	const [totalPage, setTotalPage] = useState(Math.ceil(profileList.length / row));
-
-	const splitList = () => {
-		const array = [];
-		for (let i = 0; i < totalPage; i += 1) {
-			array.push(profileList.slice(i * row, (i + 1) * row));
-		}
-		return array;
-	};
-
-	const [profileListSplit] = useState<any>(splitList()); // 10개(row)씩 나눈 리스트
-	const [searchSplit, setSearchSplit] = useState<any>(); // 검색 결과 리스트
-
-	const [data, setData] = useState(profileListSplit[0]);
-
-	const [search, setSearch] = useState({ option: 'name', input: '' });
-	const [sorting, setSorting] = useState<SortingState>([]);
-	const [currentPage, setCurrentPage] = useState(0);
+	const { profileList } = useProfile();
 	const columnHelper = createColumnHelper<profileListType>();
-
-	const previousPage = () => {
-		setCurrentPage(currentPage - 1);
-	};
-
-	const nextPage = () => {
-		setCurrentPage(currentPage + 1);
-	};
-
-	const changePage = (page: number) => {
-		setCurrentPage(page);
-	};
-
-	const [customPagination, setCustomPagination] = useState<any[]>();
-
-	useMemo(() => {
-		const arr = [];
-		for (let i = 0; i < totalPage; i += 1) {
-			arr[i] = i;
-		}
-		setCustomPagination(arr);
-	}, [totalPage]);
+	const [search, setSearch] = useState({ option: 'name', input: '' });
+	const [filter, setFilter] = useState<any>();
+	const [show] = useState(10);
 
 	const addTag = (value: any) => {
 		return <p className="text-md font-bold">{value}</p>;
@@ -93,67 +52,32 @@ const Workforce = () => {
 		}),
 	];
 
-	const table = useReactTable({
-		data,
-		columns,
-		state: {
-			sorting,
-		},
-		onSortingChange: setSorting,
-		getCoreRowModel: getCoreRowModel(),
-		getSortedRowModel: getSortedRowModel(),
-		debugTable: true,
-	});
-
-	const onSearch = (e: any) => {
-		const { id, value } = e.target;
-		setSearch({ ...search, [id]: value });
-	};
+	const searchItem = [
+		{ option: 'name', value: '이름' },
+		{ option: 'rank', value: '직급' },
+		{ option: 'task', value: '부서' },
+		{ option: 'place', value: '근무지' },
+	];
 
 	useEffect(() => {
-		if (search.input) {
-			setData(searchSplit[currentPage]);
-		} else {
-			setData(profileListSplit[currentPage]);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [currentPage]);
-
-	useEffect(() => {
-		let filter;
 		switch (search.option) {
 			case 'name':
-				filter = profileList.filter((item) => item.name.includes(search.input));
+				setFilter(profileList.filter((item) => item.name.includes(search.input)));
 				break;
 			case 'position':
-				filter = profileList.filter((item) => item.position.includes(search.input));
+				setFilter(profileList.filter((item) => item.position.includes(search.input)));
 				break;
 			case 'rank':
-				filter = profileList.filter((item) => item.rank.includes(search.input));
+				setFilter(profileList.filter((item) => item.rank.includes(search.input)));
 				break;
 			case 'task':
-				filter = profileList.filter((item) => item.task.includes(search.input));
+				setFilter(profileList.filter((item) => item.task.includes(search.input)));
 				break;
 			case 'place':
-				filter = profileList.filter((item) => item.place.includes(search.input));
+				setFilter(profileList.filter((item) => item.place.includes(search.input)));
 				break;
 			default:
 				break;
-		}
-		if (filter) {
-			setCurrentPage(0);
-			if (search.input) {
-				const array = [];
-				for (let i = 0; i < Math.ceil(filter.length / row); i += 1) {
-					array.push(filter.slice(i * row, (i + 1) * row));
-				}
-				setSearchSplit(array);
-				setData(array[0]);
-			} else {
-				setData(profileListSplit[currentPage]);
-			}
-
-			setTotalPage(Math.ceil(filter.length / row));
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [search]);
@@ -165,107 +89,15 @@ const Workforce = () => {
 					<div className="text-xl font-bold text-navy-700 dark:text-white">인력사항</div>
 				</header>
 
-				<div className="mt-8 mx-[3rem] min-h-[600px]">
-					<table className="w-full">
-						<thead>
-							{table.getHeaderGroups().map((headerGroup) => (
-								<tr key={headerGroup.id} className="!border-px !border-gray-400">
-									{headerGroup.headers.map((header) => {
-										return (
-											<th
-												key={header.id}
-												colSpan={header.colSpan}
-												onClick={header.column.getToggleSortingHandler()}
-												className="cursor-pointer border-b-[1px] border-gray-200 pt-4 pb-2 pr-4 text-start"
-												style={{ width: header.getSize() }}
-											>
-												<div className="items-center justify-between text-xs text-gray-200">
-													<p className="text-lg font-bold text-gray-600 dark:text-white">
-														{header.id}
-														{{
-															asc: '▲',
-															desc: '▼',
-														}[header.column.getIsSorted() as string] ?? null}
-													</p>
-												</div>
-											</th>
-										);
-									})}
-								</tr>
-							))}
-						</thead>
-						<tbody>
-							{table.getRowModel().rows.map((row) => {
-								return (
-									<tr key={row.id}>
-										{row.getVisibleCells().map((cell) => {
-											return (
-												<td key={cell.id} className="border-white/0 py-3 pr-4">
-													{flexRender(cell.column.columnDef.cell, cell.getContext())}
-												</td>
-											);
-										})}
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
-				</div>
-
-				<Flex className="mt-[2rem] mx-[2rem]">
-					{/* 검색 */}
-					<InputGroup className="mb-2">
-						<div className="!w-[8rem]">
-							<Select id="option" defaultValue="name" onChange={(e) => onSearch(e)}>
-								<option value="name">이름</option>
-								<option value="position">직책</option>
-								<option value="rank">직급</option>
-								<option value="task">부서</option>
-								<option value="place">근무지</option>
-							</Select>
-						</div>
-						<Input id="input" className="ml-[2rem]" onChange={(e) => onSearch(e)} />
-						<InputRightElement>
-							<SearchIcon />
-						</InputRightElement>
-					</InputGroup>
-					<Spacer />
-
-					{/* 페이지 */}
-					<div className="w-full  flex justify-center sm:justify-end flex-col sm:flex-row gap-5 items-center">
-						<div className="flex">
-							<ul className="flex justify-center items-center gap-x-[10px] z-30" role="navigation" aria-label="Pagination">
-								<li
-									className={` prev-btn flex items-center justify-center w-[36px] rounded-[6px] h-[36px] border-[1px] border-solid border-[#E4E4EB] disabled] ${
-										currentPage === 0 ? 'bg-[#cccccc] pointer-events-none' : ' cursor-pointer'
-									}`}
-									onClick={previousPage}
-								>
-									<MdChevronLeft />
-								</li>
-								{customPagination?.map((_data, index) => (
-									<li
-										className={`flex items-center justify-center w-[36px] rounded-[6px] h-[34px] border-[1px] border-solid border-[2px] bg-[#FFFFFF] cursor-pointer ${
-											currentPage === index ? 'text-blue-600  border-sky-500' : 'border-[#E4E4EB] '
-										}`}
-										onClick={() => changePage(index)}
-										key={_data}
-									>
-										{index + 1}
-									</li>
-								))}
-								<li
-									className={`flex items-center justify-center w-[36px] rounded-[6px] h-[36px] border-[1px] border-solid border-[#E4E4EB] ${
-										currentPage === totalPage - 1 ? 'bg-[#cccccc] pointer-events-none' : ' cursor-pointer'
-									}`}
-									onClick={nextPage}
-								>
-									<MdChevronRight />
-								</li>
-							</ul>
-						</div>
-					</div>
-				</Flex>
+				<ColumnsTable
+					columns={columns}
+					searchItem={searchItem}
+					list={profileList}
+					show={show}
+					search={search}
+					setSearch={setSearch}
+					filter={filter}
+				/>
 			</Card>
 		</div>
 	);
