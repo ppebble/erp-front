56a4a02b8b417ad9fde6c3b4c2e328082455128a
HyeunUk <huju@nexmore.co.kr>
2024-05-31 16:18:35 +0900

    -

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -12,8 +12,6 @@ import {
 	Input,
 	Textarea,
 	Text,
-	Container,
-	Card,
 } from '@chakra-ui/react';
 import useModal from '../../store/useModal';
 import ToastEditor from '../textEditor/toastEditor';
