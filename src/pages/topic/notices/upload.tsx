f511dd3df2ec2b63fcc981bc2cf7f2e514d84cd3
HyeunUk <huju@nexmore.co.kr>
2024-06-03 18:30:20 +0900

    axios upload 모듈 적용

--- src/pages/topic/notices/upload.tsx
+++ src/pages/topic/notices/upload.tsx
@@ -1,16 +1,38 @@
 import { useEffect, useState } from 'react';
 import InputContainer from '../../../components/inputContainer';
+import { testParam } from '../../../network/commonResult';
+import { TestUploadService } from '../../../services/testUploadService';
 
 const Upload = () => {
 	const [fileCount, setFileCount] = useState(0);
 	const [fileValue, setFileValue] = useState<any>();
+	const service = TestUploadService().fileUpload;
 
-	useEffect(() => {
+	const upload = () => {
+		const test: testParam = { name: '테스트', age: '11' };
+		const formData = new FormData();
+
 		console.log(fileValue);
-	}, [fileValue]);
+		fileValue.forEach((v: any) => {
+			console.log(v);
+			formData.append('uploadFiles', v[0]);
+		});
+		formData.append('data', JSON.stringify(test));
+		// // eslint-disable-next-line no-restricted-syntax
+		// for (const key of formData.keys()) {
+		// 	console.log(key);
+		// }
+		//
+		// // eslint-disable-next-line no-restricted-syntax
+		// for (const value of formData.values()) {
+		// 	console.log(value);
+		// }
 
+		service.mutate(formData);
+	};
 	return (
 		<div>
+			<button onClick={upload}>업로드</button>
 			<InputContainer props={{ id: fileCount, file: '' }} count={fileCount} setCount={setFileCount} setValue={setFileValue} type="attachment" />
 		</div>
 	);
